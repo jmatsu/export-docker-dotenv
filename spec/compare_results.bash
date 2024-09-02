6dfe14e7de7ad8c66222b5bf4f06c52fe72474ec
Jumpei Matsuda <jmatsu.drm@gmail.com>
2024-09-02 14:23:27 +0900

    test: Reverse env files to create non-overwritten variables

--- spec/compare_results.bash
+++ spec/compare_results.bash
@@ -3,6 +3,7 @@
 set -eux
 
 readonly shell_path="${1?shell is required}"; shift
+readonly env_mode="${1---export}"; shift
 readonly env_files=("$@")
 
 # shellcheck disable=SC2155
@@ -15,8 +16,17 @@ if ((${#env_files[*]} == 0)); then
   exit 1
 fi
 
-spec/create_expected_results.bash "${env_files[@]}" > "$expected_result_path"
-spec/create_actual_results.bash "$shell_path" "${env_files[@]}" > "$actual_result_path"
+if [[ "$env_mode" == "--no-overwrite" ]]; then
+  args=()
+  for ((i = $((${#env_files[@]} - 1)); i >= 0; i--)); do
+      args+=(${env_files[i]})
+  done
+else
+  args=("${env_files[@]}")
+fi
+
+spec/create_expected_results.bash "$env_mode" "${args[@]}" > "$expected_result_path"
+spec/create_actual_results.bash "$shell_path" "${args[@]}" > "$actual_result_path"
 
 if diff --strip-trailing-cr "$expected_result_path" "$actual_result_path"; then
   echo 'ok'
